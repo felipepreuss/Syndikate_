3d2b5a10eeb2c55bd6300eb6af913350fb6f3f7e
preuss <87501978+felipepreuss@users.noreply.github.com>
2024-08-06 18:50:42 -0300

    Add files via upload

---
+++ Syndikate_/mainmenu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+func _on_new_pressed():
+	get_tree().change_scene_to_file("res://node_2d.tscn") # Replace with function body.
+
+
+
+func _on_quit_pressed():
+	get_tree().quit() # Replace with function body.
