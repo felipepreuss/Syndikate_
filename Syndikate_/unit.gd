18cd1d9566dd41ad892491600d1bcc9ef51c8373
preuss <87501978+felipepreuss@users.noreply.github.com>
2024-08-08 16:29:16 -0300

    Add files via upload

--- Syndikate_/unit.gd
+++ Syndikate_/unit.gd
@@ -17,15 +17,38 @@ var hit = false
 @export var damage = 10
 @export var price = 50
 @export var enemy : bool
-
+ 
 var target
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	pass # Replace with function body.
+	atk_dmg.body_entered.connect(on_attack_damage_body_entered)
+	atk_range.body_entered.connect(on_attack_range_body_entered)
+	atk_range.body_exited.connect(on_attack_range_body_exited) # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
+	match current_state:
+		WALK:
+			walk_state(delta)
+		ATTACK:
+			attack_state(delta)
+		HURT:
+			hurting_state(delta)
+	
+#States
+func walk_state(delta):
+	pass
+func attack_state(delta):
+	pass
+func hurting_state(delta):
+	pass
+func on_attack_damage_body_entered():
+	pass
+	
+func on_attack_range_body_entered():
+	pass
+
+func on_attack_range_body_exited():
 	pass
